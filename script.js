8dcfade6cadecafa2837fd0bb52858b9614ba2dc
Matthew Gunderson <matthew.gunderson12@gmail.com>
2019-06-03 09:47:52 -0700

    adjusted speed

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-var c =  canvas.getContext('2d');
+var c = canvas.getContext('2d');
 
 
 function Circle(x, y, dx, dy, radius) {
@@ -12,30 +12,30 @@ function Circle(x, y, dx, dy, radius) {
   this.dx = dx;
   this.dy = dy;
   this.radius = radius;
-  
+
   //drawing circle
-  this.draw = function() {
+  this.draw = function () {
     c.beginPath();
     c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
     c.strokeStyle = 'blue';
     c.stroke();
     c.fill();
   }
-  
+
   //changing props
-  this.update = function() {
-  //x axis
-    if( this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+  this.update = function () {
+    //x axis
+    if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
       this.dx = -this.dx;
-  }
-  //y axis
-  if( this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+    }
+    //y axis
+    if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
       this.dy = -this.dy;
-  }
-    
-  this.x += this.dx;
-  this.y += this.dy;
-    
+    }
+
+    this.x += this.dx;
+    this.y += this.dy;
+
     this.draw();
   }
 }
@@ -43,13 +43,13 @@ function Circle(x, y, dx, dy, radius) {
 //drawing 100 cricles w/random variables and positions
 var circleArray = [];
 
-for(var i = 0; i < 100; i++) {
+for (var i = 0; i < 100; i++) {
   var radius = 30;
   var x = Math.random() * (innerWidth - radius * 2) + radius;
   var y = Math.random() * (innerHeight - radius * 2) + radius;
-  var dx = (Math.random() - 0.5) * 2;
-  var dy = (Math.random() - 0.5) * 2;
-  
+  var dx = (Math.random() - 0.5) * 3;
+  var dy = (Math.random() - 0.5) * 3;
+
   circleArray.push(new Circle(x, y, dx, dy, radius));
 }
 
@@ -59,8 +59,8 @@ for(var i = 0; i < 100; i++) {
 function animate() {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, innerWidth, innerHeight);
-  
-  for(var i = 0; i < circleArray.length; i++) {
+
+  for (var i = 0; i < circleArray.length; i++) {
     circleArray[i].update();
   }
 }
